982fe029613c595897e2a8f9c748f8c83aac84c7
Gary Guo <gary@garyguo.net>
2021-03-11 05:02:54 +0000

    Allow customisable muntjac_core DataWidth
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rtl/muntjac_core.sv
+++ rtl/muntjac_core.sv
@@ -1,6 +1,9 @@
 `include "tl_util.svh"
 
 module muntjac_core import muntjac_pkg::*; #(
+  // Bus width
+  parameter DataWidth = 64,
+
   // Number of bits of physical address supported. This must not exceed 56.
   // This must match AddrWidth of the TileLink interface.
   parameter PhysAddrLen = 56,
@@ -15,7 +18,7 @@ module muntjac_core import muntjac_pkg::*; #(
     input  logic            rst_ni,
 
     // Memory interface
-    `TL_DECLARE_HOST_PORT(64, PhysAddrLen, SourceWidth, SinkWidth, mem),
+    `TL_DECLARE_HOST_PORT(DataWidth, PhysAddrLen, SourceWidth, SinkWidth, mem),
 
     input  logic irq_software_m_i,
     input  logic irq_timer_m_i,
@@ -28,7 +31,7 @@ module muntjac_core import muntjac_pkg::*; #(
     output instr_trace_t dbg_o
 );
 
-  `TL_DECLARE(64, PhysAddrLen, SourceWidth, SinkWidth, mem);
+  `TL_DECLARE(DataWidth, PhysAddrLen, SourceWidth, SinkWidth, mem);
   `TL_BIND_HOST_PORT(mem, mem);
 
   localparam [SourceWidth-1:0] DcacheSourceBase = 0;
@@ -61,8 +64,9 @@ module muntjac_core import muntjac_pkg::*; #(
       .dbg_o
   );
 
-  `TL_DECLARE_ARR(64, PhysAddrLen, SourceWidth, SinkWidth, ch, [3:0]);
+  `TL_DECLARE_ARR(DataWidth, PhysAddrLen, SourceWidth, SinkWidth, ch, [3:0]);
   tl_socket_m1 #(
+    .DataWidth(DataWidth),
     .AddrWidth (PhysAddrLen),
     .SourceWidth (SourceWidth),
     .SinkWidth (SinkWidth),
@@ -80,6 +84,90 @@ module muntjac_core import muntjac_pkg::*; #(
     `TL_CONNECT_HOST_PORT(device, mem)
   );
 
+  `TL_DECLARE(64, PhysAddrLen, SourceWidth, SinkWidth, dcache_mem);
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (DataWidth),
+    .HostAddrWidth (PhysAddrLen),
+    .DeviceAddrWidth (PhysAddrLen),
+    .HostSourceWidth (SourceWidth),
+    .DeviceSourceWidth (SourceWidth),
+    .HostSinkWidth (SinkWidth),
+    .DeviceSinkWidth (SinkWidth),
+    .HostMaxSize (6),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) dcache_mem_adapter (
+    .clk_i,
+    .rst_ni,
+    `TL_CONNECT_DEVICE_PORT(host, dcache_mem),
+    `TL_CONNECT_HOST_PORT_IDX(device, ch, [0])
+  );
+
+  `TL_DECLARE(64, PhysAddrLen, SourceWidth, SinkWidth, dcache_ptw);
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (DataWidth),
+    .HostAddrWidth (PhysAddrLen),
+    .DeviceAddrWidth (PhysAddrLen),
+    .HostSourceWidth (SourceWidth),
+    .DeviceSourceWidth (SourceWidth),
+    .HostSinkWidth (SinkWidth),
+    .DeviceSinkWidth (SinkWidth),
+    .HostMaxSize (3),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) dcache_ptw_adapter (
+    .clk_i,
+    .rst_ni,
+    `TL_CONNECT_DEVICE_PORT(host, dcache_ptw),
+    `TL_CONNECT_HOST_PORT_IDX(device, ch, [2])
+  );
+
+  `TL_DECLARE(64, PhysAddrLen, SourceWidth, SinkWidth, icache_mem);
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (DataWidth),
+    .HostAddrWidth (PhysAddrLen),
+    .DeviceAddrWidth (PhysAddrLen),
+    .HostSourceWidth (SourceWidth),
+    .DeviceSourceWidth (SourceWidth),
+    .HostSinkWidth (SinkWidth),
+    .DeviceSinkWidth (SinkWidth),
+    .HostMaxSize (6),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) icache_mem_adapter (
+    .clk_i,
+    .rst_ni,
+    `TL_CONNECT_DEVICE_PORT(host, icache_mem),
+    `TL_CONNECT_HOST_PORT_IDX(device, ch, [1])
+  );
+
+  `TL_DECLARE(64, PhysAddrLen, SourceWidth, SinkWidth, icache_ptw);
+  tl_adapter #(
+    .HostDataWidth (64),
+    .DeviceDataWidth (DataWidth),
+    .HostAddrWidth (PhysAddrLen),
+    .DeviceAddrWidth (PhysAddrLen),
+    .HostSourceWidth (SourceWidth),
+    .DeviceSourceWidth (SourceWidth),
+    .HostSinkWidth (SinkWidth),
+    .DeviceSinkWidth (SinkWidth),
+    .HostMaxSize (3),
+    .DeviceMaxSize (6),
+    .HostFifo (1'b0),
+    .DeviceFifo (1'b0)
+  ) icache_ptw_adapter (
+    .clk_i,
+    .rst_ni,
+    `TL_CONNECT_DEVICE_PORT(host, icache_ptw),
+    `TL_CONNECT_HOST_PORT_IDX(device, ch, [3])
+  );
+
   muntjac_icache #(
     .PhysAddrLen (PhysAddrLen),
     .SourceWidth (SourceWidth),
@@ -91,8 +179,8 @@ module muntjac_core import muntjac_pkg::*; #(
     .rst_ni,
     .cache_h2d_i (icache_h2d),
     .cache_d2h_o (icache_d2h),
-    `TL_CONNECT_HOST_PORT_IDX(mem, ch, [1]),
-    `TL_CONNECT_HOST_PORT_IDX(mem_ptw, ch, [3])
+    `TL_CONNECT_HOST_PORT(mem, icache_mem),
+    `TL_CONNECT_HOST_PORT(mem_ptw, icache_ptw)
   );
 
   muntjac_dcache #(
@@ -106,8 +194,8 @@ module muntjac_core import muntjac_pkg::*; #(
     .rst_ni,
     .cache_h2d_i (dcache_h2d),
     .cache_d2h_o (dcache_d2h),
-    `TL_CONNECT_HOST_PORT_IDX(mem, ch, [0]),
-    `TL_CONNECT_HOST_PORT_IDX(mem_ptw, ch, [2])
+    `TL_CONNECT_HOST_PORT(mem, dcache_mem),
+    `TL_CONNECT_HOST_PORT(mem_ptw, dcache_ptw)
   );
 
 endmodule
