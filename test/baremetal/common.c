0084bcff95d8661d6de715872873a8c0148732c6
Luís Marques <luismarques@lowrisc.org>
2022-05-03 13:51:15 +0200

    Fix the constant used to set mcountinhibit
    mcountinhibit is a 32-bit register but it was being set with a 64-bit
    constant literal. Clang would complain when that literal value was
    assigned to an unsigned int variable. Fix this by using the appropriate
    32-bit constant instead.
    
    Signed-off-by: Luís Marques <luismarques@lowrisc.org>

--- test/baremetal/common.c
+++ test/baremetal/common.c
@@ -14,7 +14,7 @@ void pcount_reset() {
 
 void pcount_enable(int enable) {
   // Note cycle is disabled with everything else
-  unsigned int inhibit_val = enable ? 0x0 : 0xFFFFFFFFFFFFFFFF;
+  unsigned int inhibit_val = enable ? 0x0 : 0xFFFFFFFF;
   // CSR 0x320 was called `mucounteren` in the privileged spec v1.9.1, it was
   // then dropped in v1.10, and then re-added in v1.11 with the name
   // `mcountinhibit`. Unfortunately, the version of binutils we use only allows
